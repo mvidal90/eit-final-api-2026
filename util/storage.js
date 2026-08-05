7493e724ceb61a1e41e320ae0afe0c7f5954281a
Marcos Vidal <devtionit@gamil.com>
2026-08-05 19:08:57 -0300

    ADD restore .env to .gitignore, create Readme.md for temp images, and fix filename generation in storage

--- util/storage.js
+++ util/storage.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
         cb(null, uploadPath)
     },
     filename: (req, file, cb) => {
-        cb(null, `${file.fieldname}-${Date.now()}.${path.extname(file.originalname)}`)
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
     }
 })
 
